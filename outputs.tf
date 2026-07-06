44a611701f94a79ed345a78515f00acd07925ac7
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "cosmosdb_sql_dedicated_gateways" {
   description = "All cosmosdb_sql_dedicated_gateway resources"
   value       = azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways
 }
+output "cosmosdb_sql_dedicated_gateways_cosmosdb_account_id" {
+  description = "List of cosmosdb_account_id values across all cosmosdb_sql_dedicated_gateways"
+  value       = [for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : v.cosmosdb_account_id]
+}
+output "cosmosdb_sql_dedicated_gateways_instance_count" {
+  description = "List of instance_count values across all cosmosdb_sql_dedicated_gateways"
+  value       = [for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : v.instance_count]
+}
+output "cosmosdb_sql_dedicated_gateways_instance_size" {
+  description = "List of instance_size values across all cosmosdb_sql_dedicated_gateways"
+  value       = [for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : v.instance_size]
+}
 
