842284f8661d9d0e4b92875bf0f5571384868e92
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cosmosdb_sql_dedicated_gateways_id" {
+  description = "Map of id values across all cosmosdb_sql_dedicated_gateways, keyed the same as var.cosmosdb_sql_dedicated_gateways"
+  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.id }
+}
 output "cosmosdb_sql_dedicated_gateways_cosmosdb_account_id" {
   description = "Map of cosmosdb_account_id values across all cosmosdb_sql_dedicated_gateways, keyed the same as var.cosmosdb_sql_dedicated_gateways"
   value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.cosmosdb_account_id }
