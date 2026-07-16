e9797428cce9ad31962efe236474baf521822885
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "cosmosdb_sql_dedicated_gateways_id" {
   description = "Map of id values across all cosmosdb_sql_dedicated_gateways, keyed the same as var.cosmosdb_sql_dedicated_gateways"
-  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.id }
+  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "cosmosdb_sql_dedicated_gateways_cosmosdb_account_id" {
   description = "Map of cosmosdb_account_id values across all cosmosdb_sql_dedicated_gateways, keyed the same as var.cosmosdb_sql_dedicated_gateways"
-  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.cosmosdb_account_id }
+  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.cosmosdb_account_id if v.cosmosdb_account_id != null && length(v.cosmosdb_account_id) > 0 }
 }
 output "cosmosdb_sql_dedicated_gateways_instance_count" {
   description = "Map of instance_count values across all cosmosdb_sql_dedicated_gateways, keyed the same as var.cosmosdb_sql_dedicated_gateways"
-  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.instance_count }
+  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.instance_count if v.instance_count != null }
 }
 output "cosmosdb_sql_dedicated_gateways_instance_size" {
   description = "Map of instance_size values across all cosmosdb_sql_dedicated_gateways, keyed the same as var.cosmosdb_sql_dedicated_gateways"
-  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.instance_size }
+  value       = { for k, v in azurerm_cosmosdb_sql_dedicated_gateway.cosmosdb_sql_dedicated_gateways : k => v.instance_size if v.instance_size != null && length(v.instance_size) > 0 }
 }
 
